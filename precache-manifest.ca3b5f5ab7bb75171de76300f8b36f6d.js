c1680af536d918e51d72cfc5f821b78561149a14
aaroncaraway <aaroncaraway42@gmail.com>
2020-01-03 15:14:37 -0800

    Updates

---
+++ precache-manifest.ca3b5f5ab7bb75171de76300f8b36f6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a1d3c398e1426037814a411a04a0470",
+    "url": "/moovieranker/index.html"
+  },
+  {
+    "revision": "8e3f01b22f9d63c74435",
+    "url": "/moovieranker/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f6deea45defd7298840f",
+    "url": "/moovieranker/static/js/2.742e2c72.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/moovieranker/static/js/2.742e2c72.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8e3f01b22f9d63c74435",
+    "url": "/moovieranker/static/js/main.e70c10e6.chunk.js"
+  },
+  {
+    "revision": "c98350afcf35e3fa28b8",
+    "url": "/moovieranker/static/js/runtime-main.2f6ab886.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/moovieranker/static/media/logo.5d5d9eef.svg"
+  }
+]);
